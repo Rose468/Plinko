2ae076ce66b1a6188d839d348ea28d181b1c674c
Rose468 <lifenature776@gmail.com>
2021-08-02 00:11:38 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,52 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var divisionHeight = 300;
+var divisions = [];
+var plinkos = [];
+var particles = [];
+
+
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(480,800);
+ // createSprite(400, 200, 50, 50);
+ engine = Engine.create();
+ world = engine.world;
+ 
+ 
+ ground = new Ground(600,height,1200,20);
+ 
+
+ for(var k = 0; k <=width; k = k +80){
+ divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight));
+//divisions[k].display();
+}
+
+for (var j = 40; j <=width; j=j+50)
+{
+  plinkos.push(new Plinko(j,75));
+}
+
+for(var j = 15; j <=width-10; j = j+50)
+{
+  plinkos.push(new Plinko(j,175))
+}
+ 
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
+  text(mouseX + "," + mouseY , 20,20);
+Engine.update(engine);
+
+ground.display();
+
+
+
   drawSprites();
+
 }
